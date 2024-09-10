159571de63587c3e650c7b138ba9b0f8c4fe862d
Acelio Filho <86500305+acelio-filho@users.noreply.github.com>
2024-09-09 23:39:55 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -1,55 +1,60 @@
 
+// lista de sites wordpress a acessar via api rest, separados por assunto
 
-//sites
-//https://sonymusic.com.br/
-//https://rifferama.com/
-//sites no wordpress.com https://public-api.wordpress.com/rest/v1.1/sites/dominio-do-site/posts/?number=2&pretty=true
-//parâmetro preatty=false vem em json e imagens em base64... bem extenso o arquivo.
+/* temos dois tipos de sites wordpress:
+ 1 sites hospedados no wordpress.com 
+ 2- sites que utilizam o CMS wordpress em seus servidores próprios
+ 
+ cada um tem uma chamada endpoint diferente:
 
-// Preencher este objeto com os endpoints reais
-// export const assuntosEndpoints = {
-//   // Exemplo:
-//   musica: {
-//     site1: 'https://rifferama.com/wp-json/wp/v2/posts/?_embed&per_page=3',
-//     site2: 'https://sonymusic.com.br/wp-json/wp/v2/posts/?_embed&per_page=3',
-//     // ...
-//   },
-//   // ...
-// };
+ 1- endpoint [wordpress.com]: https://public-api.wordpress.com/rest/v1.1/sites/NOME-DO-SITE.wordpress.com/posts/?_embed&&orderby=date&order=desc&per_page=3&limit=3
+ para reconhecer se o site está sob o domínio wordpress.com, existe uma chave no objeto wpcom:true. ou apenas verificar se a chave existe, dentro de [meta] -> (posts.meta.wpcom)
+ 
+ 2- endpoint wordpress: https://DOMÍNIO-DO-SITE/wp-json/wp/v2/posts/?_embed&per_page=3
+ os endpoints têm diferenças no recurso per_page e _embed
+ não consigo acessar featured_media_src_url em nenhum dos dois tipos de endpoints.
+ featured_media retorna a ID da mídia
+ Acredito ser outra chamada para obter a featured_media_src_url do post usando endpoint /midia/?featured_media no lugar de /posts/?
 
-// export const assuntosEndpoints2 = {
-//     cinema: {
-//       site1: 'https://public-api.wordpress.com/rest/v1.1/sites/cinemacompoesia.wordpress.com/posts/?number=2&pretty=true',
-//       site2: 'https://sonymusic.com/wp-json/wp/v2/posts/?_embed&per_page=3',
-//       // ...
-//     },
-//     // ...
-//   };
-//   //
+ as chaves e valores desejados são:  content | exerpt | per_page=3 | featured_media(featured_media_src_url) |  link(link para o site), URL(link para o post)
 
-export const listaDeAssuntos = ['Cinema', 'Música', 'Cultura'];
+- preciso encontrar também o "nome do site e a url da home dele(link)", para ter um h1 dentro da section, antes de mostrar os posts daquele site, informando a fonte, colocando dentro de uma div
+
+Important: o parâmetro _fields permite chamar somente o que se quer, evitando dados desnecessários.
+Retorna um objeto com outra estrutura. bem menor e mais rápido. 
+ex: ?_fields=author,id,content,excerpt,title,link,featured_media, guid
+pega a featured_media(id da mídia) e faz outra requisição pra chamar a imagem
+
+/wp-json/wp/v2/media/?_fields=guid&id=featured_media. //gui retorna a url da imagem de acordo com a id, que vem de featured_media na requisição anterior
+
+*/
+
+//lista de assuntos
+export const listaDeAssuntos = ['Cinema', 'Música', 'Notícias'];
 // sitesWordPress
   export const assuntosEndpoints = {
     cinema: [
       {
         nome: 'Cinema Com Poesia',
         url: 'https://cinemacompoesia.wordpress.com',
-        endpoint: 'https://public-api.wordpress.com/rest/v1.1/sites/cinemacompoesia.wordpress.com/posts/?_embed&&orderby=date&order=desc&per_page=3'
+        endpoint: 'https://public-api.wordpress.com/rest/v1.1/sites/cinemacompoesia.wordpress.com/posts/?_embed&_embedded&orderby=date&order=desc&per_page=3&limit=3',
+        endpointImg: 'https://public-api.wordpress.com/rest/v1.1/sites/cinemacompoesia.wordpress.com/media/?_fields=guid&id='
       }
     ],
     música: [ // estou usando com acento gráfico propositalmente
         {
           nome: 'Rifferama',
           url: 'https://rifferama.com',
-          endpoint: 'https://rifferama.com/wp-json/wp/v2/posts/?_embed&per_page=3'
+          endpoint: 'https://rifferama.com/wp-json/wp/v2/posts/?_embed&_fields=author,id,excerpt,title,link,featured_media&_embeded&per_page=3',
+          endpointImg:'https://rifferama.com/wp-json/wp/v2/media/?_fields=guid&id='
         },
         {
-            nome: 'Sony Music Brasil',
+            nome: 'Sony Music Brasil', /* este site não usa posts - está aqui somente para exemplo e teste do laço forEach */
             url: 'https://www.sonymusic.com.br',
-            endpoint: 'https://www.sonymusic.com.br/wp-json/wp/v2/posts/?_embed&_fields=author,id,excerpt,title,link,featured_media_src_url,featured_media&_embeded&per_page=3'
+            endpoint: 'https://www.sonymusic.com.br/wp-json/wp/v2/posts/?_embed&_fields=author,id,excerpt,title,link,featured_media&_embeded&per_page=3'
         }
       ],
-      cultura:[
+      notícias:[ // estou usando com acento gráfico propositalmente
         {
         nome: 'Revista Veja',
         url: 'https://veja.abril.com.br/cultura/',
@@ -58,7 +63,7 @@ export const listaDeAssuntos = ['Cinema', 'Música', 'Cultura'];
         },{
           nome: 'Catraca Livre',
           url:'https://catracalivre.com.br/',
-          endpoint:'https://catracalivre.com.br/wp-json/wp/v2/posts/?per_page=3',
+          endpoint:'https://catracalivre.com.br/wp-json/wp/v2/posts/?_embeded&per_page=3',
         }
       ]
       //outros temas
--- scripts-gemini.js
+++ scripts-gemini.js
@@ -7,21 +7,23 @@ if(nomeDaPessoa===null || nomeDaPessoa===""){nomeDaPessoa="Anônimo"};
 localStorage.setItem("nomeDaPessoa", nomeDaPessoa);}
 document.getElementById('nomeDaPessoa').innerText=nomeDaPessoa;
 const ultimoAssunto = localStorage.getItem('ultimoAssunto');
-if(!ultimoAssunto){ultimoAssunto="música"}// estou usando com acento gráfico propositalmente
+if(!ultimoAssunto){ultimoAssunto="música"}//abre sempre com o assunto música
 
     
 
 // Carregar os assuntos na lista select
 const assuntoSelect = document.getElementById('assunto');
+
 listaDeAssuntos.forEach(assunto => {
   const option = document.createElement('option');
   option.value = assunto.replace(" ", "-").toLowerCase();
   option.text = assunto;
   assuntoSelect.appendChild(option);
 });
-// Marcar a opção selecionada antes, se houver
+// Marcar a opção selecionada antes
 if (ultimoAssunto) {
     assuntoSelect.value = ultimoAssunto.replace(" ", "-").toLowerCase();
+    capturarEndpoints(assuntoSelect.value);
   }
 
 // Função para capturar endpoints do assunto selecionado
@@ -50,6 +52,9 @@ function capturarEndpoints(assunto="") {
   buscarEExibirPosts(endpoints);
 
 }
+  //pega os dados do site
+  const linkSIte = endPoint.url;
+  const titleSIte = endPoint.nome;
 
 // Função para buscar e exibir os posts de um conjunto de endpoints
 async function buscarEExibirPosts(endpoints) {
@@ -60,7 +65,7 @@ async function buscarEExibirPosts(endpoints) {
     for (const endpoint of endpoints) {
       try {
         const response = await fetch(endpoint.endpoint); console.log("log response: ");console.log(response);
-        const posts = await response.json(); console.log("log posts: ");console.log(posts);
+        const posts = await response.json(); console.log("log posts: ");console.table(posts);
   
         (function(){
  
@@ -68,7 +73,7 @@ async function buscarEExibirPosts(endpoints) {
           const urlSite = posts;
            
           /*definimos um endpoint para resgatar os posts, com imagens*/
-          const endPoint = `${urlSite}/wp-json/wp/v2/posts?_embed`;
+          const endPoint = `${urlSite}/wp-json/wp/v2/posts?_embed$per_page=3`;
            
           /*faz requisição do tipo GET que retornará os posts*/
           fetch(endPoint)
@@ -76,36 +81,67 @@ async function buscarEExibirPosts(endpoints) {
           .then( posts => {
            
              /*Define uma variável que receberá um array*/
-              const html = [];
-             
+             const html = [];
+           
             /*Percorre o posts*/
             posts.forEach( post => {
                    
                 /*resgata o titulo*/
                 const title = post.title.rendered;
-                 
+                console.log(title);
                 /*resgatar o link*/
-                const link = post.link;
+                const link = post.link;console.log(link);
            
                 /*resgata as images*/
-                const images = post._embedded['wp:featuredmedia'][0].media_details.sizes; 
+                const images = post._embedded['wp:featuredmedia'][0].media_details.sizes; console.log(images);// esta chave muda de nome 
            
                 //resgata a image especifica, no caso medium.
-                const image = images.medium.source_url;
-                      
+                const image = images.medium.source_url; console.log(image);
+                
+              template=`<article id="post${title[i]}${post.id}">
+               <h4><a href="${posts.posts[0].URL}">${posts.posts[0].title}</a></h4>
+                 <img src="${posts.posts.featured_media[0]}" alt="${posts.posts[0].title}" style="width:100%; height:auto;">
+                 <p>${posts.posts.excerpt}</p>
+                 <p><a target="noticia-completa" href="${posts.posts[0].URL}">Leia mais</a></h4></p>
+             </article><hr />` 
+                return template;
              });
-           
+             
           });
            
           })();
+          /*template para cada site | var endpoints
+
+          <div id="site${posts.site[i]}"><p class="nome-site"><span>CONTEÚDO DO SITE</span> <a href="${posts.link}" title="veja mais no site ${posts.nomeSite}" class="link-site">${post.nomeSite}</a></p>
+          
+          //loop forEach para cada endpoint
+          
+          //template para cada post 
+          <article id="post${posts.site[1]}${posts.post[0].postID}">
+              <h4><a href="${posts.posts[0].URL}">${posts.posts[0].title}</a></h4>
+                <img src="${posts.posts.featured_media[0]}" alt="${posts.posts[0].title}" style="width:100%; height:auto;">
+                <p>${posts.posts.excerpt}</p>
+                <p><a target="noticia-completa" href="${posts.posts[0].URL}">Leia mais</a></h4></p>
+            </article>
+            //template post fim
+
+            //loop forEach endpoint fim
+
+          </div> //template cada site fim | var endpoints
+        
+*/
+        //mudar a função abaixo para adaptar ao template acima 
+
+        
+
         // Itera sobre cada post e renderiza
         // se for um array, ou um array vazio usamos forEach
         if(posts.constructor === Array || posts.length<1){
             posts.forEach(post => {
                 const article = document.createElement('article');
                 article.innerHTML = `<h4><a href="${post.link}">${post.title.rendered}</a></h4>
-              <img src="${post._links["wp:attachment"][0].href}" alt="${post.title.rendered}" style="width:100%; height:auto;">
-              <div>${post.content.rendered}</div>
+              <img src="${post.featured_media}" alt="${post.title.rendered}" style="width:100%; height:auto;">
+              <div>${post.excerpt.rendered}</div>
             `;
             resultadosDiv.appendChild(article);
           });}else{ //se não for array
@@ -113,15 +149,15 @@ async function buscarEExibirPosts(endpoints) {
             if(posts.meta.wpcom){ //testa se é 
                 const article = document.createElement('article'); console.log(posts.URL);
                 article.innerHTML = `<h4><a href="${posts.posts[0].URL}">${posts.posts[0].title}</a></h4>
-                <img src="${posts.posts.featured_image}" alt="${posts.posts[0].title}" style="width:100%; height:auto;">
+                <img src="${posts.posts.featured_media}" alt="${posts.posts[0].title}" style="width:100%; height:auto;">
                 <p>${posts.posts.excerpt}</p>
                 <p><a target="noticia-completa" href="${posts.posts[0].URL}">Leia mais no ${posts}</a></h4></p>`;
                 resultadosDiv.appendChild(article);
             }else{
             const article = document.createElement('article'); console.log(posts.link);
                 article.innerHTML = `<h4><a href="${posts.link}">${posts.title.rendered}</a></h4>
-              <img src="${featured_media_src_url}" alt="${posts.title.rendered}" style="width:100%; height:auto;">
-              <p>${posts.content.rendered}</p>
+              <img src="${posts.featured_media_src_url}" alt="${posts.title.rendered}" style="width:100%; height:auto;">
+              <p>${posts.excerpt.rendered}</p>
               <p><a target="noticia-completa" href="${posts.posts.URL}">Leia mais no ${posts}</a></h4></p>
               `;
             resultadosDiv.appendChild(article);
